c25baf57bf1965fb436cf72b46a13bdc9643cc9e
acostyle <acostyle.m.z@protonmail.com>
2021-10-20 11:53:51 +0600

    chore: edit settings of project

--- bake_cake/settings.py
+++ bake_cake/settings.py
@@ -128,12 +128,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
