66f04352110ada87a8089f05ee0d4701e6c3cfc8
Yuriy Belov <paganism@mail.ru>
2021-10-21 16:49:26 +0300

    feat: add order cancellation

--- cake/urls.py
+++ cake/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('order_cake/', views.order_cake, name='order-cake')
+    path('order_cake/', views.order_cake, name='order-cake'),
+    path('cancel_order/<int:pk>', views.cancel_order, name='cancel_order'),
 ]
--- cake/views.py
+++ cake/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from cake.models import Order
+from django.shortcuts import reverse
 
 
 def index(request):
@@ -6,3 +10,13 @@ def index(request):
 
 def order_cake(request):
     return render(request, 'order_custom_cake.html')
+
+
+@login_required(login_url='/users/login/')
+def cancel_order(request, pk):
+    if request.method == 'POST':
+        user = request.user
+        order = get_object_or_404(Order, id=pk)
+        order.status = 'CANCELLED'
+        order.save()
+        return HttpResponseRedirect(reverse('profile', args=[user.id]))
