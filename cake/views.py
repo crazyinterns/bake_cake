37142239e4f8b18a36303009133085ff68b72664
acostyle <acostyle.m.z@protonmail.com>
2021-10-21 18:56:27 +0600

    feat(cake): add view to order_cake

--- cake/views.py
+++ cake/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from .forms import OrderForm
 
 def index(request):
     return render(request, 'index.html')
 
 def order_cake(request):
-    return render(request, 'order_custom_cake.html')
+    submitted = False
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/order_cake?submitted=True')
+    else:
+        form = OrderForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'order_custom_cake.html', {'form': form, 'submitted': submitted})
