7c3749c719a021825d3556d40f576326729c88bb
Yuriy Belov <paganism@mail.ru>
2021-10-21 22:15:02 +0300

    feat: feat_add_permament_redirect_to_main

--- bake_cake/urls.py
+++ bake_cake/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('', include('cake.urls')),
+    path('', RedirectView.as_view(url='/index/', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
