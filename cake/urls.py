e489ff1c21fbac1b72faf13b99795ebc0a4e10a7
Medet <56687178+acostyle@users.noreply.github.com>
2021-10-22 11:20:54 +0600

    feat(cake): add DateTimePicker (#27)

--- cake/urls.py
+++ cake/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
+from django.views.i18n import JavaScriptCatalog
 from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('order_cake/', views.order_cake, name='order-cake'),
+    path('jsi18n', JavaScriptCatalog.as_view(), name='js-catalog'),
     path('cancel_order/<int:pk>', views.cancel_order, name='cancel_order'),
 ]
