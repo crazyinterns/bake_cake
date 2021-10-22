096062cf57e5032cae5ae0b8fc5352ddc3a79e6a
Yuriy Belov <paganism@mail.ru>
2021-10-22 18:29:46 +0300

    refactor: fix create cake form

--- cake/views.py
+++ cake/views.py
@@ -11,11 +11,28 @@ def index(request):
 
 @login_required(login_url='/users/login/')
 def order_cake(request):
+    user = request.user
     submitted = False
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
-            form.save()
+
+            cleaned_data = form.cleaned_data
+
+            order = Order(
+                layer=cleaned_data['layer'],
+                form=cleaned_data['form'],
+                promocode=cleaned_data['promocode'],
+                customer=user,
+                address=cleaned_data['address'],
+                delivery_at=cleaned_data['delivery_at'],
+                comment=cleaned_data['comment'],
+                writing=cleaned_data['writing']               
+            )
+            order.save()
+            order.berry.set(cleaned_data['berry']),
+            order.decoration.set(cleaned_data['decoration']),
+            order.topping.set(cleaned_data['topping']),
             return HttpResponseRedirect('/order_cake?submitted=True')
     else:
         form = OrderForm
