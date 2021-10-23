68c2fc023caf66207f765bbf4da2292e3b7ab074
Medet <56687178+acostyle@users.noreply.github.com>
2021-10-23 10:44:18 +0600

    feat: add price after order (#34)
    * feat(cake): add price calculation
    
    * template: add price

--- cake/views.py
+++ cake/views.py
@@ -1,9 +1,12 @@
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from cake.models import Order
 from django.shortcuts import reverse
 from .forms import OrderForm
+from users.views import serialize_order
 
 
 def index(request):
@@ -33,13 +36,17 @@ def order_cake(request):
             order.berry.set(cleaned_data['berry']),
             order.decoration.set(cleaned_data['decoration']),
             order.topping.set(cleaned_data['topping']),
+         
             return HttpResponseRedirect('/order_cake?submitted=True')
     else:
         form = OrderForm
         if 'submitted' in request.GET:
             submitted = True
+    
+    orders = list(user.orders.exclude(status='CANCELLED').select_related('layer','form') \
+                .prefetch_related('berry','decoration','topping').order_by('-id'))
 
-    return render(request, 'order_custom_cake.html', {'form': form, 'submitted': submitted})
+    return render(request, 'order_custom_cake.html', {'form': form, 'submitted': submitted, 'page_obj': [serialize_order(order) for order in orders]})
 
 
 @login_required(login_url='/users/login/')
