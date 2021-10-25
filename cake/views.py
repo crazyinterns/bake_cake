cca65bab2d6c9e4db700fd5ccb2396fa18d70b35
acostyle <acostyle.m.z@protonmail.com>
2021-10-25 17:12:48 +0600

    refactor(cake): refactor code

--- cake/views.py
+++ cake/views.py
@@ -1,24 +1,22 @@
-from django.conf import settings
-from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponseRedirect
+
 from cake.models import Order
-from django.shortcuts import reverse
 from .forms import OrderForm
 from users.views import serialize_order, get_order_price
-import json
 
 
 def index(request):
     return render(request, 'index.html')
+
 
 @login_required(login_url='/users/login/')
 def order_cake(request):
     user = request.user
     submitted = False
     if request.method == 'POST':
-        
+
         form = OrderForm(request.POST)
         if form.is_valid():
 
@@ -33,16 +31,16 @@ def order_cake(request):
                 delivery_at=cleaned_data['delivery_at'],
                 comment=cleaned_data['comment'],
                 writing=cleaned_data['writing'],
-                fixed_price=0        
+                fixed_price=0,
             )
-            
+      
             order.save()
             order.berry.set(cleaned_data['berry']),
-            order.decoration.set(cleaned_data['decoration']),
-            order.topping.set(cleaned_data['topping']),
+            order.decoration.set(cleaned_data['decoration'])
+            order.topping.set(cleaned_data['topping'])
 
-            order.fixed_price=get_order_price(order)
-            
+            order.fixed_price = get_order_price(order)
+
             order.save()
 
             return HttpResponseRedirect('/order_cake?submitted=True')
@@ -50,11 +48,21 @@ def order_cake(request):
         form = OrderForm
         if 'submitted' in request.GET:
             submitted = True
-    
-    orders = list(user.orders.exclude(status='CANCELLED').select_related('layer','form') \
-                .prefetch_related('berry','decoration','topping').order_by('-id'))
 
-    return render(request, 'order_custom_cake.html', {'form': form, 'submitted': submitted, 'page_obj': [serialize_order(order) for order in orders]})
+    orders = list(
+        user.orders.exclude(status='CANCELLED')\
+        .select_related('layer', 'form')\
+        .prefetch_related('berry', 'decoration', 'topping')\
+        .order_by('-id'),
+    )
+
+    context = {
+        'form': form,
+        'submitted': submitted,
+        'page_obj': [serialize_order(order) for order in orders],
+    }
+
+    return render(request, 'order_custom_cake.html', context=context)
 
 
 @login_required(login_url='/users/login/')
